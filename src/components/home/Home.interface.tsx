dd1f33d2a7f2b9cddb979ccaa546f83832db1f74
Rav Sohal <ravinder.sohal@bp.com>
2024-09-05 01:57:13 +0100

    update home component with data as intial pass

---
+++ src/components/home/Home.interface.tsx
@@ -0,0 +1,18 @@
+export interface IHomeData {
+  url: string;
+  name: string;
+  region: string;
+  coatOfArms: string;
+  words: string;
+  titles: string[];
+  seats: string[];
+  currentLord: string;
+  heir: "";
+  overlord: string;
+  founded: string;
+  founder: string;
+  diedOut: string;
+  ancestralWeapons: string[];
+  cadetBranches: string[];
+  swornMembers: string[];
+}
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,10 +1,48 @@
+import React from "react";
+import { IHomeData } from "./Home.interface";
+
 const HomeComponent = () => {
+  // BaseUrl
+  const baseUrl = "https://www.anapioficeandfire.com/api/houses";
+
+  // State
+  const [data, setData] = React.useState<IHomeData[]>([]);
+  const [error, setError] = React.useState(null);
+  const [loading, setLoading] = React.useState<boolean>(true);
+
+  // GetData function
+  const getData = async () => {
+    try {
+      const response = await fetch(baseUrl);
+      if (!response.ok) {
+        throw new Error("Network response was not ok.");
+      }
+      const data = await response.json();
+      setData(data);
+      setLoading(false);
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
+
+  //Lifecycle hook to getData onMount
+  React.useEffect(() => {
+    getData();
+  }, []);
+
+  //Render the view
   return (
-    <>
-      <button type="button" className="btn btn-primary">
-        Homepage
-      </button>
-    </>
+    <div className="container mt-5">
+      <h1>Home</h1>
+      {loading ? (
+        <>Loading...</>
+      ) : error ? (
+        <>Error {error}</>
+      ) : (
+        <>{JSON.stringify(data)}</>
+      )}
+    </div>
   );
 };
 
